8264c35736fbab7bb51034d7268dfad5d395ef56
Andrzej <andrzmichal@gmail.com>
2019-07-08 22:10:51 +0200

    Create basic menu and fill tabs context
    The basic context of flat pages are created. Moreover links to main menu
    are implemented. Scope of backend service is created. At last new footer
    is added.

--- app/app.py
+++ app/app.py
@@ -3,9 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def blog():
+def index():
     return render_template("index.html")
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
